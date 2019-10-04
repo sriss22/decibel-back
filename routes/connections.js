f5fb40e137d3610517605b1c16fea8d4e8228f92
Sam Riss <samriss22@gmail.com>
2019-10-03 22:01:30 -0500

    master: finished up work on routes. routes for listing connections, testing a connection, and listing out the audit for a connection. tested in postman. need to write unit tests

--- routes/connections.js
+++ routes/connections.js
@@ -4,9 +4,20 @@ var pgp = require('pg-promise')(/* no options */);
 var router = express.Router();
 var decibelAppDB = pgp('postgres://decibel:admin1234@localhost:5432/decibel-app');
 
+/* Helper function to build connection string for a db connection */
+function buildConnection(req, res, next) {
+    var user = req.body.user;
+    var pass = req.body.pass;
+    var host = req.body.host;
+    var port = req.body.port;
+    var dbName = req.body.db;
+    req.connectionString = `postgres://${user}:${pass}@${host}:${port}/${dbName}`;
+    next();
+}
+
 /* GET list - list out all connections */
 router.get('/list', function(req, res, next) {
-    decibelAppDB.any('SELECT * from connections')
+    decibelAppDB.any('SELECT * FROM connections')
         .then(function(data) {
             res.json({
                 'results': data,
@@ -17,21 +28,16 @@ router.get('/list', function(req, res, next) {
         })
         .catch(function(error) {
             next(createError(500, error.message));
-        });
+        })
+        .finally(decibelAppDB.$pool.end);
 });
 
 /* POST test - test a connection using credentials passed in */
-router.post('/test', function(req, res, next) {
-    var user = req.body.user;
-    var pass = req.body.pass;
-    var host = req.body.host;
-    var port = req.body.port;
-    var db = req.body.db;
-    var connectionString = `postgres://${user}:${pass}@${host}:${port}/${db}`;
-    var db = pgp(connectionString);
+router.post('/test', buildConnection, function(req, res, next) {
+    var db = pgp(req.connectionString);
 
     db.connect()
-        .then(function (obj) {
+        .then(function(obj) {
             obj.done();
             res.json({
                 'results': 'Successfully logged in!',
@@ -42,21 +48,29 @@ router.post('/test', function(req, res, next) {
         })
         .catch(function (error) {
             next(createError(500, error.message));
-        });
+        })
+        .finally(db.$pool.end);
 })
 
-/* POST :id/audit - list out audit records for a connection AFTER date passed in (frontend current time) and update last checked audit connection */
-router.post('/:id/audit', function(req, res, next) {
-    console.log(req.params);
-    // @TODO: Get audit records based on date passed in and connection info (need to think on this)
-    // @TODO: Update last checked audit timestamp for connection
-    // @TODO: Return audit records
-    res.json({
-        'results': 'results go here!',
-        'error': false,
-        'errorMessage': null,
-        'status': 200
-    });
+/* POST :id/audit - list out audit records for a connection in date range passed in */
+router.post('/:id/audit', buildConnection, function(req, res, next) {
+    var db = pgp(req.connectionString);
+    var begin = req.body.begin;
+    var end = req.body.end;
+
+    db.any('SELECT * FROM db_audit WHERE occurred_at >= ${begin} AND occurred_at <= ${end}', { begin, end })
+        .then(function(data) {
+            res.json({
+                'results': data,
+                'error': false,
+                'errorMessage': null,
+                'status': 200
+            });
+        })
+        .catch(function(error) {
+            next(createError(500, error.message));
+        })
+        .finally(db.$pool.end);
 });
 
 module.exports = router;
