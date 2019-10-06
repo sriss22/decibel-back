d449e4db69bb61e1c3aff7f1cb1b2fd607a96d8a
Sam Riss <samriss22@gmail.com>
2019-10-05 20:22:21 -0500

    master: fixed typo in sql file

--- sql_scripts/test2-db.sql
+++ sql_scripts/test2-db.sql
@@ -1,7 +1,7 @@
 create table ttbl2 (id serial primary key, value text);
 alter table ttbl2 add column updated_at timestamp default current_timestamp;
 insert into ttbl2 (value) values ('test2 value'), ('another test value'), ('a third value');
-delete from ttb2 where id = 3;
+delete from ttbl2 where id = 3;
 insert into ttbl2 (value) values ('a value goes here');
 
 create user testuser2 with password 'user2pass';
